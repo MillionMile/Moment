d7da4cb5e69504445a5ddb2c1dd639622a6ade96
MillionMile <38458586+MillionMile@users.noreply.github.com>
2018-06-02 18:47:28 +0800

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,8 +1,12 @@
 const  app=require("express");
 const commentCon=require("./commentCON");
+const remenCON=require("./remenCON");
+const voteCON=require("./voteCON");
 
 module.exports=function () {
   let router=app.Router();
   router.use('/commentList',commentCon());
+  router.use('/remen',remenCON());
+  router.use('/vote',voteCON());
   return router;
 };
