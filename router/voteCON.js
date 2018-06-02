9766cc3ad925f98c84cac6492cceae2f767946b7
MillionMile <38458586+MillionMile@users.noreply.github.com>
2018-06-02 19:27:43 +0800

    Update voteCON.js

--- router/voteCON.js
+++ router/voteCON.js
@@ -2,18 +2,6 @@ const app = require('express');
 const operationDao = require("../dao/OperationDao.js");
 module.exports = function () {
     let router = app.Router();
-    router.get('/', (req, res) => {
-        operationDao.OperationsCount(
-            { user_id: "5b04375510fe443358c0cf63" },
-            { picture: "5b07fe58960ddc17d404f8d6" },
-            { vote: { $exists: true } },
-            (err, res) => {
-                if (err)
-                    console.log("根据Id查找失败", err);
-                else
-                    console.log("根据Id查找成功", res);
-            })
-    });
 
     router.get('/addVote', (req, res) => {
         let picture_id = req.query.Picture_id;
