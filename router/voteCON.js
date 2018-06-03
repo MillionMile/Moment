5bd3a4dc7a45bd35757e497d5680c5d8f12d532b
MillionMile <38458586+MillionMile@users.noreply.github.com>
2018-06-03 16:15:42 +0800

    Update voteCON.js

--- router/voteCON.js
+++ router/voteCON.js
@@ -1,30 +1,37 @@
 const app = require('express');
 const operationDao = require("../dao/OperationDao.js");
+
 module.exports = function () {
     let router = app.Router();
 
-    router.get('/addVote', (req, res) => {
+    router.get('/addVote', (req, res) => {//创建点赞(user_id,picture_id)
         let picture_id = req.query.Picture_id;
-        let user_id = "5b12287a9217ec0c481447b5";
-
+        let user_id = req.session['user_id'];
         operationDao.create({
             user_id: user_id,
             picture: picture_id,
             vote: {},
-        }, (err, res) => {
+        }, (err, result) => {
+            if (err)
+                res.json({ "result": "图片插入失败" + err })
+            else
+                res.json({ "result": "图片插入成功" + result });
         });
     });
 
-
-    router.get('/delVote', (req, res) => {
+    router.get('/delVote', (req, res) => {//取消点赞(user_id,picture_id)
         let picture_id = req.query.Picture_id;
-        let user_id = "5b12287a9217ec0c481447b5";
+        let user_id = req.session['user_id'];
         operationDao.remove({
             user_id: user_id,
             picture: picture_id,
-            vote:{$exists:true},
-        }, (err, res) => {
+            vote: { $exists: true },
+        }, (err, result) => {
+            if (err)
+                res.json({ "result": "图片插入失败" + err })
+            else
+                res.json({ "result": "图片插入成功" + result });
         });
-    })
+    });
     return router;
 };
