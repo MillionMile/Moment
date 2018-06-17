ad6b36bcddb17aae4a17889798913464a40345da
1124443512@qqcom <1124443512@qq.com>
2018-06-17 11:49:39 +0800

    Moment-modify-6/17

--- router/router.js
+++ router/router.js
@@ -8,24 +8,15 @@ const userCON = require('./userCON')
 module.exports=function () {
   let router=app.Router();
 
-    // router.use(function (req,res,next) {
-    //     //模拟登录session
-    //     req.session.user_id = "5b04375510fe443358c0cf63";
-    //     req.session.username="test2";
-
-    //     if(!req.session['user_id'] && req.url !== '/login'){
-    //         res.redirect('/xxxxx');//到时在写
-    //     }else{
-    //         next();
-    //     }
-    // });
-    router.use('/commentsList', commentCON());
-    router.use('/picturesList', listCON());
-    router.use('/vote', voteCON());
-	router.use('/favor', favorCON());
-	router.use('/user', userCON())
+    router.use('/user', userCON())
+    router.use('/commentsList', commentCON())
+    router.use('/picturesList', listCON())
+    router.use('/vote', voteCON())
+    router.use('/favor', favorCON())
     router.use('/',(req,res)=>{
-        res.render("index", { isLogin: !!req.session["user_id"] });
-    });
-  return router;
-};
+        res.render("index", { isLogin: !!req.session["user_id"] })
+    })
+
+  return router
+
+}
